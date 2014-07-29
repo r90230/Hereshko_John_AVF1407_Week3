c0c7eb82be8bf97b6a85326ea5ce1966df1a5121
John Hereshko <JayHereshko@fullsail.edu>
2014-07-29 19:57:02 -0400

    Issue with location?

--- Resources/app.js
+++ Resources/app.js
@@ -6,97 +6,216 @@ var currentTime = new Date();
 var year = currentTime.getFullYear();
 var month = currentTime.getMonth() + 1;
 var day = currentTime.getDate();
+var workingDate = year+""+month+""+day;
+var today = workingDate.toString();
 
-var mountainView = Map.createAnnotation({
-    latitude:37.390749,
-    longitude:-122.081651,
-    title:"Appcelerator Headquarters",
-    subtitle:'Mountain View, CA',
-    pincolor:Map.ANNOTATION_RED,
-    myid:1
-});
+<<<<<<< HEAD
+var myCoords = [];
 
-var currentLocation = Map.createAnnotation({
-	latitude:null,
-	longitude:null,
-	title:null,
-	subtitle:null,
-	pincolor:Map.ANNOTATION_RED,
-	myid:2
-});
 
-var makingPins = function(e){
-	Map.createAnnotation({
-	    latitude:37.390749,
-	    longitude:-122.081651,
-	    title:"Appcelerator Headquarters",
-	    subtitle:'Mountain View, CA',
-	    pincolor:Map.ANNOTATION_RED 
-	});
-};
 
-var mapview = Map.createView({
-    mapType: Map.NORMAL_TYPE,
-    region: {latitude:33.74511, longitude:-84.38993,
-            latitudeDelta:0.01, longitudeDelta:0.01},
-    animate:true,
-    regionFit:true,
-    userLocation:true,
-    annotations:[mountainView]
-});
+var getCoords = function(day){
+=======
 
-win.add(mapview);
-// Handle click events on any annotations on this map.
-mapview.addEventListener('click', function(evt) {
-    Ti.API.info("Annotation " + evt.title + " clicked, id: " + evt.annotation.myid);
-});
-
-var url;
+	
 
 var getCoords = function(){
+>>>>>>> FETCH_HEAD
 	if (Ti.Geolocation.locationServicesEnabled) {
 	    Ti.Geolocation.purpose = 'Get Current Location';
 	    Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
 	    Ti.Geolocation.distanceFilter = 10;
 	    Ti.Geolocation.preferredProvider = Ti.Geolocation.PROVIDER_GPS;
+	    
+	    var storedCoords = [];
+<<<<<<< HEAD
+=======
+	    
+	   var gpsProvider = Ti.Geolocation.Android.createLocationProvider({
+	    name: Ti.Geolocation.PROVIDER_GPS,
+	    minUpdateTime: 60, 
+	    minUpdateDistance: 100
+		});
+		
+		Ti.Geolocation.Android.addLocationProvider(gpsProvider);
+			
+>>>>>>> FETCH_HEAD
 	 
 	    Ti.Geolocation.addEventListener('location', function(e) {
 	        if (e.error) {
 	            alert('Error: ' + e.error);
 	        } else {
-	           mapview.region = e.coords;
-	           Ti.Geolocation.reverseGeocoder(e.coords.latitude, e.coords.longitude, function(z){
-	           		var currentLocation = Map.createAnnotation({
-					latitude:e.coords.latitude,
-					longitude:e.coords.longitude,
-					title:z.places[0].street,
-					subtitle:z.places[0].zipcode,
-					pincolor:Map.ANNOTATION_RED,
-					myid:2
-					});
-	           	mapview.annotations = [mountainView,currentLocation];
+<<<<<<< HEAD
+	        	var myLat = e.coords.latitude;
+	        	var myLng = e.coords.longitude;
+=======
+	        	
+	        	gpsProvider = Ti.Geolocation.Android.createLocationProvider({
+			    name: Ti.Geolocation.PROVIDER_GPS,
+			    minUpdateTime: 60, 
+			    minUpdateDistance: 100
+				});
+
+				Ti.Geolocation.Android.addLocationProvider(gpsProvider);
+>>>>>>> FETCH_HEAD
+	         	
+	           	var mountainView = Map.createAnnotation({
+			    latitude:28.578216,
+			    longitude:-81.308165,
+			    title:"Full Sail University",
+			    subtitle:'Winter Park, FL',
+			    pincolor:Map.ANNOTATION_BLUE
+			});
+				storedCoords.push(mountainView);
+<<<<<<< HEAD
+				
+				var url = "https://api.foursquare.com/v2/venues/search?ll="+myLat+","+myLng+"&intent=browse&radius=800&limit=10&client_id=3IDALSAWRC1OYQDFCB5SMTJEQ4NKPJWDGLYWX1HRQQGPSGIW&client_secret=IKQNC01TZRJRKWWZFELXJS0ETSVQPDIHGUJ1D2JFHBN43Z3P&v="+today;
+				console.log(url);
+				
+   					xhr.open("GET", url);
+					xhr.send();
+			
+				if(xhr.DONE){        	
+	            var db = Ti.Database.open('geoCoords');
+	           	var dbRows = db.execute('SELECT * from coords');
+	            };
+=======
+	           	
+	          	           	
+	           	var db = Ti.Database.open('geoCoords');
+	           	var dbRows = db.execute('SELECT * from coords');
+	           
+>>>>>>> FETCH_HEAD
+	           	
+	           	while(dbRows.isValidRow()){
+	           		
+	           	var dbLat = dbRows.fieldByName('lat');
+	           	var dbLng = dbRows.fieldByName('lng');
+	           	var dbTitle = dbRows.fieldByName('title');	
+	           	
+	           	var annotations = Map.createAnnotation({
+	           		latitude: dbLat,
+	           		longitude: dbLng,
+	           		title: dbTitle,
+	           		pincolor: Map.ANNOTATION_PURPLE,
+	           		animate: true
 	           	});
-	        }
-	    });
-	    exports.getCoords = getCoordsCode;
+	           	storedCoords.push(annotations);
+	           	dbRows.next();
+	           	};
+<<<<<<< HEAD
+	          
+=======
+>>>>>>> FETCH_HEAD
+	           	var mapview = Map.createView({
+			    mapType: Map.NORMAL_TYPE,
+			    region: e.coords,
+			    animate:true,
+			    regionFit:true,
+			    userLocation:true,
+				});
+				mapview.setAnnotations(storedCoords);
+				win.add(mapview);
+				
+				mapview.addEventListener('click', function(evt) {
+<<<<<<< HEAD
+    				var detailView = Ti.UI.createView({});
+				});
+=======
+    				var detailView = Ti.UI.createView({
+    					
+    				});
+});
+	        
+>>>>>>> FETCH_HEAD
+	    }
+	});//end event listener
 		} else {
 	   		 alert('Please enable location services');
 				}
 };
 
-
-var url = "https://api.foursquare.com/v2/venues/search?ll=40.3142890930176,-76.6989822387695&client_id=3IDALSAWRC1OYQDFCB5SMTJEQ4NKPJWDGLYWX1HRQQGPSGIW&client_secret=IKQNC01TZRJRKWWZFELXJS0ETSVQPDIHGUJ1D2JFHBN43Z3P&v=" + year+""+month+""+day;
-console.log(exports.getCoords);
+<<<<<<< HEAD
+=======
+var url = "https://api.foursquare.com/v2/venues/search?ll=40.3142890930176,-76.6989822387695&client_id=3IDALSAWRC1OYQDFCB5SMTJEQ4NKPJWDGLYWX1HRQQGPSGIW&client_secret=IKQNC01TZRJRKWWZFELXJS0ETSVQPDIHGUJ1D2JFHBN43Z3P&v=20140726";
 
 var remoteResponse = function(e){
 	var json = JSON.parse(this.responseText);
+	
+	var db = Ti.Database.open('geoCoords');
+	var dbTableCreate = db.execute('CREATE TABLE IF NOT EXISTS coords(id INTEGER PRIMARY KEY, lat REAL, lng REAL, title TEXT);');
+	db.close();
+	
+	for(i=0;i<29;i++){ 
+		
+	if(Ti.Network.online){
+		try 
+		{var placesLat = json.response.venues[i].location.lat;
+		var placesLng = json.response.venues[i].location.lng;
+		var placesTitle = json.response.venues[i].name;
+		} catch (e){
+			alert("There was an issue pulling in " + e);
+		};
+	
+		var db = Ti.Database.open('geoCoords');
+		var insertRows = db.execute('INSERT INTO coords (lat,lng,title) VALUES (?,?,?)', placesLat, placesLng, placesTitle);
+		var lastRow = db.lastInsertRowId;
+		
+		
+		db.close();
+		};
 	};
+};
+>>>>>>> FETCH_HEAD
 
 var remoteError = function(e){
 	Ti.API.debug("Status: " + this.status);
 	Ti.API.debug("Response: " + this.responseText);
 	Ti.API.debug("Error: " + e.error);
-	alert("There was an error.");
+	alert("There was an error. " +e.error);
+};
+
+var remoteResponse = function(e){
+	var json = JSON.parse(this.responseText);
+	
+	var db = Ti.Database.open('geoCoords');
+	var dbTableCreate = db.execute('CREATE TABLE IF NOT EXISTS coords(id INTEGER PRIMARY KEY, lat REAL, lng REAL, title TEXT);');
+	db.close();
+	
+	for(i=0;i<9;i++){ 
+		
+	if(Ti.Network.online){
+		var db = Ti.Database.open('geoCoords');
+		try 
+		{var placesLat = json.response.venues[i].location.lat;
+		var placesLng = json.response.venues[i].location.lng;
+		var placesTitle = json.response.venues[i].name;
+		} catch (e){
+			var isNull = function(data) {
+   				return (data === null || data === undefined || data === "");
+		};
+			
+			if (json.response.venues[i].location.hasOwnProperty("lat")) {
+   				isNull(json.response.venues[i].location.lat) ? Ti.API.info("No lat value!") : Ti.API.info("Has lat value:  " + json.response.venues[i].location.lat);
+			}
+			if (json.response.venues[i].location.hasOwnProperty("lng")) {
+			    isNull(json.response.venues[i].location.lng) ? Ti.API.info("No lng value!") : Ti.API.info("Has lng value:  " + json.response.venues[i].location.lng);
+			}
+			if (json.response.venues[i].hasOwnProperty("name")) {
+			    isNull(json.response.venues[i].name) ? Ti.API.info("No name value!") : Ti.API.info("Has name value:  " + json.response.venues[i].name);
+			}
+		};//end catch
+	
+		
+		var insertRows = db.execute('INSERT INTO coords (lat,lng,title) VALUES (?,?,?)', placesLat, placesLng, placesTitle);
+		var lastRow = db.lastInsertRowId;
+		console.log(placesLng);
+		console.log(i);
+		
+		
+		db.close();
+		};
+	};
 };
 
 var xhr = Ti.Network.createHTTPClient({
@@ -105,9 +224,11 @@ var xhr = Ti.Network.createHTTPClient({
 	timeout: 20000
 });
 
-xhr.open("GET", url);
-xhr.send();
 
-console.log(currentLocation.title);
+<<<<<<< HEAD
+getCoords(today);
+win.open();
+=======
 getCoords();
 win.open();
+>>>>>>> FETCH_HEAD
